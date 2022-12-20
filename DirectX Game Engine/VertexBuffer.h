2759cbf08caac26c0090ecd509944b531e78a94b
JoeZ <joesmejko@gmail.com>
2022-12-20 08:35:18 +0100

    A

--- DirectX Game Engine/VertexBuffer.h
+++ DirectX Game Engine/VertexBuffer.h
@@ -4,12 +4,13 @@
 class VertexBuffer
 {
 public:
-	bool load(void* list_verticies, UINT size_vertex, UINT size_list);
+	bool load(void* list_verticies, UINT size_vertex, UINT size_list, void* shader_byte_code, UINT size_byte_shader);
 	bool release();
 private:
 	UINT m_size_vertex;
 	UINT m_size_list;
 private:
 	ID3D11Buffer* m_buffer;
+	ID3D11InputLayout* m_layout;
 };
 
